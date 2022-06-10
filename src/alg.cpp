3f3c8febd6d2e651276d35ccb36707b947c0ec50
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-06-10 20:45:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,47 @@
-#include <cmath>
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
+
 bool checkPrime(uint64_t value) {
-int count = 0;
-for (uint64_t i = 2; i < sqrt(value); i++) {
-if ((value % i) == 0) {
-return false;
-}
-if (value > 1) {
-for (int x = 2; x <= sqrt(value); x++)
-if (value % x == 0) {
-return false;
-}
-return true;
-}
-else {
-return false;
-}
-return true;
+    int i;
+    if(value < 2)
+            return 0;
+    for(i = 2; i <= value; i++) {
+        
+        if(value%i == 0)
+            if(value != i)
+                return 0;
+    }
+    return 1;
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t x = 0;
-for (int i = 0; ; i++) {
-if (checkPrime(i)) {
-if (checkPrime(i) == true) {
-x++;
-}
-if (x == n) {
-return i;
-}
-else {
-continue;
-}
-}
-return 0;
+    int i=0, c=0;
+    while(true) {
+        i++;
+        if(checkPrime(i))
+            c++;
+        if(c==n)
+            return i;
+    }
+    return c;
 }
 
 uint64_t nextPrime(uint64_t value) {
-for (uint64_t i = value + 1; ; i++) {
-if (checkPrime(i))
-return i;
-}
+    int a=value+1;
+    while(true) {
+        if(checkPrime(a))
+            return a;
+        a++;
+    }
+    return a;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t sum = 0;
-if (hbound > 2) {
-for (int i = 2; i < hbound; i++) {
-if (checkPrime(i)) {
-sum = sum + i;
-}
-}
-}
-return sum;
+    int sum=0,i;
+    for(i = 0; i<hbound; i++)
+    if(checkPrime(i))
+        sum+=i;
+    return sum;
 }
